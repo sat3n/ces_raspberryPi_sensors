c264018997e1f45f4bd9c86108c726b06a348321
sat3n <satenderkundu@hotmail.com>
2020-03-08 23:49:51 +0000

    adding bit operators and alarmtoggle(half finished)

--- test.cpp
+++ test.cpp
@@ -29,6 +29,7 @@
 #include</usr/include/arm-linux-gnueabihf/sys/ioctl.h>
 #include<linux/i2c.h>
 #include<linux/i2c-dev.h>
+#include <bits/stdc++.h>
 #define BUFFER_SIZE 0x12      //0x00 to 0x12
 
 using namespace std;
@@ -42,7 +43,67 @@ int bcdToDec(char b) {
 char decToBcd (int b) {
 	return (((b/10) << 4) | (b % 10));
 	}
+	
+int modifyBit(int n, int p, int b) 
+{ 
+    int mask = 1 << p; 
+    return (n & ~mask) | ((b << p) & mask); 
+} 
 char dataBuffer[BUFFER_SIZE];
+// Decimal to Binary conversion
+int dToB(int x){
+int remainder;
+int i=1;
+int bin_value=0;
+
+while(x!=0){
+    remainder = x%2;
+    x /= 2;
+    bin_value += remainder*i;
+    i *= 10;
+}
+return bin_value;
+}
+// Binary to Decimal conversion
+int bToD(int n) 
+{ 
+     
+    int dec_value = 0; 
+    int base = 1; 
+    int temp = n; 
+    
+    while (temp) { 
+        int last_digit = temp % 10; 
+        temp = temp / 10; 
+        dec_value += last_digit * base; 
+        base = base * 2; 
+    } 
+  
+    return dec_value; 
+}
+
+void togglealarm1(){
+ int z =(bcdToDec(dataBuffer[0x0E]));
+ modifyBit(z, 0, 1);
+ modifyBit(z, 1, 1);
+ modifyBit(z, 2, 1);
+ modifyBit(z, 3, 1);
+ modifyBit(z, 4, 1);
+ cout << "Modified 0Eh bits : " << dToB(bcdToDec(dataBuffer[0x0E])) <<endl;
+}
+
+void disablealarms(){
+ int z =(bcdToDec(dataBuffer[0x0E]));
+ modifyBit(z, 0, 0);
+ modifyBit(z, 1, 0);
+ modifyBit(z, 2, 0);
+}
+
+void togglealarm2(){
+ int z =(bcdToDec(dataBuffer[0x0E]));
+ modifyBit(z, 0, 1);
+ modifyBit(z, 2, 1);
+}
 
 int readSensorState(){
 	
@@ -76,7 +137,7 @@ int readSensorState(){
 int writeToDevice (char data, char address){
 	
 	int file;
-    printf("Starting the Write section of the coden");
+   
     
     if((file=open("/dev/i2c-1", O_RDWR)) < 0){
       cout<<"failed to open the bus\n"<< endl;
@@ -98,12 +159,27 @@ int writeToDevice (char data, char address){
 	return 0;
 }
 
+
+
 int main(){
 	readSensorState();
-	cout << "second : " << bcdToDec(dataBuffer[0x00]) 
-	<<" Hours :"<< bcdToDec(dataBuffer[0x02])<<endl;
-	writeToDevice(decToBcd(18), 0x01 );
+	
+        cout<<"Starting the Write section of the code"<<endl;
+	cout <<" Hours :"<< bcdToDec(dataBuffer[0x02]) << endl;
+	
+	cout <<" Minutes:" << bcdToDec(dataBuffer[0x01])<<endl;
+        cout << " Seconds : " << bcdToDec(dataBuffer[0x00]) <<endl;
+	writeToDevice(decToBcd(2), 0x0E );
+	cout << " Fetching value from 0Eh : " << dToB(bcdToDec(dataBuffer[0x0E])) <<endl;
+	togglealarm1();
+
+	cout <<endl<<"Minutes in binary:" << dToB(bcdToDec(dataBuffer[0x01]))<<endl; 
+	
+	
+	writeToDevice(decToBcd(2), 0x01 );
 	writeToDevice(decToBcd(12), 0x02 );
+	cout <<"Modified bit output : " <<modifyBit(bcdToDec(dataBuffer[0x01]), 0, 1)<<endl;
+	//writeToDevice(decToBcd(12), 0x00 );
 	
    return 0;
 }
